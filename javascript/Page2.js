faec49da959d2e0f508324bd82a34aa56e6993bb
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2023-05-03 14:59:29 +0300

    My commit message

--- javascript/Page2.js
+++ javascript/Page2.js
@@ -670,7 +670,145 @@ let apiDoctorApi = new TempApi.DoctorApi();import TempApi from '../src/index';le
       }
     apiRoomApi.deleteroom( roomId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{   location.href= '/Page2' ;}}});};document.getElementById('izyqyg').onclick = (event) => {
     event.preventDefault();
-    {   location.href= '/addEquipment' ;}};document.getElementById('iuynuo').onclick = (event) => {
+    {   location.href= '/addEquipment' ;}};const onClickPaginationButtonitz4rs = (chunk, pagination) => {
+    for (let i = 0; i < pagination.children.length; i++) {
+      if (
+        pagination.children[i].classList.value.includes("active") === true
+      ) {
+        pagination.children[i].classList.remove("active");
+      }
+    }
+
+    let numberOfFrontButtons =  findTypeOfPaginationitz4rs(pagination);
+    pagination.children[chunk+numberOfFrontButtons-1].classList.add("active");
+
+  apiEquipmentApi.getAllequipment((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements =[...document.getElementById("itz4rs").querySelectorAll( "[dataitem='true']" )].filter(
+    (element, index, array) =>
+    !array.reduce((hasAncestorFlag, dataItem) => hasAncestorFlag || (element.compareDocumentPosition(dataItem) & Node.DOCUMENT_POSITION_CONTAINS) === 8, false)
+  );const map = new Map();[...subDataElements].forEach((element, index) => {
+        if (index >= data.length - (chunk-1)*subDataElements.length) {
+            subDataElements[index].style.display = 'none';
+        }
+        else {
+            subDataElements[index].style.display = "";
+        }
+      });data.forEach((item, i) => {
+
+        let revertIndex = data.length - i -1;
+
+        if(data.length - chunk*subDataElements.length <= revertIndex && revertIndex < data.length - (chunk-1)*subDataElements.length){
+            try { 
+      const insideSubDataElement = subDataElements[i-(chunk-1) *subDataElements.length].querySelector("[annotationname = 'eqName']");
+      if(insideSubDataElement !== null){
+        insideSubDataElement.textContent = data[data.length -i -1].eqName;
+        
+      }
+      else if(subDataElements[i-(chunk-1) *subDataElements.length].getAttribute('annotationname') === 'eqName'){
+        subDataElements[i-(chunk-1) *subDataElements.length].textContent = data[data.length -i -1].eqName;
+        
+      }
+     } catch (e) { console.log(e) };try { 
+      const insideSubDataElement = subDataElements[i-(chunk-1) *subDataElements.length].querySelector("[annotationname = 'eqType']");
+      if(insideSubDataElement !== null){
+        insideSubDataElement.textContent = data[data.length -i -1].eqType;
+        
+      }
+      else if(subDataElements[i-(chunk-1) *subDataElements.length].getAttribute('annotationname') === 'eqType'){
+        subDataElements[i-(chunk-1) *subDataElements.length].textContent = data[data.length -i -1].eqType;
+        
+      }
+     } catch (e) { console.log(e) };try { 
+      const insideSubDataElement = subDataElements[i-(chunk-1) *subDataElements.length].querySelector("[annotationname = 'eqCost']");
+      if(insideSubDataElement !== null){
+        insideSubDataElement.textContent = data[data.length -i -1].eqCost;
+        
+      }
+      else if(subDataElements[i-(chunk-1) *subDataElements.length].getAttribute('annotationname') === 'eqCost'){
+        subDataElements[i-(chunk-1) *subDataElements.length].textContent = data[data.length -i -1].eqCost;
+        
+      }
+     } catch (e) { console.log(e) };try { 
+      const insideSubDataElement = subDataElements[i-(chunk-1) *subDataElements.length].querySelector("[annotationname = 'eqFix']");
+      if(insideSubDataElement !== null){
+        insideSubDataElement.textContent = data[data.length -i -1].eqFix;
+        
+      }
+      else if(subDataElements[i-(chunk-1) *subDataElements.length].getAttribute('annotationname') === 'eqFix'){
+        subDataElements[i-(chunk-1) *subDataElements.length].textContent = data[data.length -i -1].eqFix;
+        
+      }
+     } catch (e) { console.log(e) };
+            map.set(subDataElements[i-(chunk-1) * subDataElements.length].getAttribute('id'), data[data.length-i-1])
+        }
+        
+    // Retrieve current data from local storage
+    const storedData = window.localStorage.getItem("data");
+    const currentData = storedData
+        ? new Map(JSON.parse(storedData))
+        : new Map();
+
+    // Add new data to current data
+    const newData = Array.from(map.entries());
+    newData.forEach(([key, value]) => {
+        currentData.set(key, value);
+    });
+
+    // Save updated data to local storage
+    window.localStorage.setItem(
+        "data",
+        JSON.stringify(Array.from(currentData.entries()))
+    );
+    
+    })
+    }});}
+
+    const findTypeOfPaginationitz4rs = (pagination) => {
+
+      let firstChild = pagination.children[0];
+      let secondChild = pagination.children[1];
+
+      if (
+        (firstChild.attributes.getNamedItem("pagination-first") !== null ||
+          firstChild.attributes.getNamedItem("pagination-previous") !== null) &&
+        (secondChild.attributes.getNamedItem("pagination-first") !== null ||
+          secondChild.attributes.getNamedItem("pagination-previous") !== null)
+      ) {
+        return 2;
+      } else if  (
+        (firstChild.attributes.getNamedItem("pagination-first") !== null ||
+          firstChild.attributes.getNamedItem("pagination-previous") !== null) ||
+        (secondChild.attributes.getNamedItem("pagination-first") !== null ||
+          secondChild.attributes.getNamedItem("pagination-previous") !== null)
+      ) {
+        return 1;
+      }
+      else{
+        return 0;
+      }
+
+    }
+  
+
+    const returnChunkIndexitz4rs = (chunk, numberOfPages, cause) => {
+
+      if(cause === '+'){
+        if(chunk < numberOfPages){
+          return chunk + 1;
+        }
+        else{
+          return chunk;
+        }
+      }
+      else if(cause === '-'){
+        if(chunk > 2){
+          return chunk - 1;
+        }
+        else{
+          return 1;
+        }
+      }
+    }
+  document.getElementById('iuynuo').onclick = (event) => {
     event.preventDefault();
     {  
       let transitionId = window.location.href.split('/').at(-1);
@@ -1193,47 +1331,47 @@ let apiDoctorApi = new TempApi.DoctorApi();import TempApi from '../src/index';le
     [...subDataElements].forEach((element,index) => {if(index >= data.length) subDataElements[index].style.display = 'none';})}});apiEquipmentApi.getAllequipment((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements =[...document.getElementById("itz4rs").querySelectorAll( "[dataitem='true']" )].filter(
     (element, index, array) =>
     !array.reduce((hasAncestorFlag, dataItem) => hasAncestorFlag || (element.compareDocumentPosition(dataItem) & Node.DOCUMENT_POSITION_CONTAINS) === 8, false)
-  );const map = new Map();  data.forEach((item,i) => {
+  );const map = new Map();let chunk = 1;  data.forEach((item,i) => {
     if(subDataElements.length > i)
       {
         try { 
-      const insideSubDataElement = subDataElements[i].querySelector("[annotationname = 'eqName']");
+      const insideSubDataElement = subDataElements[i-(chunk-1) *subDataElements.length].querySelector("[annotationname = 'eqName']");
       if(insideSubDataElement !== null){
         insideSubDataElement.textContent = data[data.length -i -1].eqName;
         
       }
-      else if(subDataElements[i].getAttribute('annotationname') === 'eqName'){
-        subDataElements[i].textContent = data[data.length -i -1].eqName;
+      else if(subDataElements[i-(chunk-1) *subDataElements.length].getAttribute('annotationname') === 'eqName'){
+        subDataElements[i-(chunk-1) *subDataElements.length].textContent = data[data.length -i -1].eqName;
         
       }
      } catch (e) { console.log(e) };try { 
-      const insideSubDataElement = subDataElements[i].querySelector("[annotationname = 'eqType']");
+      const insideSubDataElement = subDataElements[i-(chunk-1) *subDataElements.length].querySelector("[annotationname = 'eqType']");
       if(insideSubDataElement !== null){
         insideSubDataElement.textContent = data[data.length -i -1].eqType;
         
       }
-      else if(subDataElements[i].getAttribute('annotationname') === 'eqType'){
-        subDataElements[i].textContent = data[data.length -i -1].eqType;
+      else if(subDataElements[i-(chunk-1) *subDataElements.length].getAttribute('annotationname') === 'eqType'){
+        subDataElements[i-(chunk-1) *subDataElements.length].textContent = data[data.length -i -1].eqType;
         
       }
      } catch (e) { console.log(e) };try { 
-      const insideSubDataElement = subDataElements[i].querySelector("[annotationname = 'eqCost']");
+      const insideSubDataElement = subDataElements[i-(chunk-1) *subDataElements.length].querySelector("[annotationname = 'eqCost']");
       if(insideSubDataElement !== null){
         insideSubDataElement.textContent = data[data.length -i -1].eqCost;
         
       }
-      else if(subDataElements[i].getAttribute('annotationname') === 'eqCost'){
-        subDataElements[i].textContent = data[data.length -i -1].eqCost;
+      else if(subDataElements[i-(chunk-1) *subDataElements.length].getAttribute('annotationname') === 'eqCost'){
+        subDataElements[i-(chunk-1) *subDataElements.length].textContent = data[data.length -i -1].eqCost;
         
       }
      } catch (e) { console.log(e) };try { 
-      const insideSubDataElement = subDataElements[i].querySelector("[annotationname = 'eqFix']");
+      const insideSubDataElement = subDataElements[i-(chunk-1) *subDataElements.length].querySelector("[annotationname = 'eqFix']");
       if(insideSubDataElement !== null){
         insideSubDataElement.textContent = data[data.length -i -1].eqFix;
         
       }
-      else if(subDataElements[i].getAttribute('annotationname') === 'eqFix'){
-        subDataElements[i].textContent = data[data.length -i -1].eqFix;
+      else if(subDataElements[i-(chunk-1) *subDataElements.length].getAttribute('annotationname') === 'eqFix'){
+        subDataElements[i-(chunk-1) *subDataElements.length].textContent = data[data.length -i -1].eqFix;
         
       }
      } catch (e) { console.log(e) };
@@ -1263,4 +1401,91 @@ let apiDoctorApi = new TempApi.DoctorApi();import TempApi from '../src/index';le
     );
     
     
+  let numberOfPages = Math.ceil(data.length/subDataElements.length);
+  let pagination = document.getElementById("itz4rs").querySelector('[pagination-list="true"]');
+
+    let paginationAttributes = [
+      "pagination-first",
+      "pagination-last",
+      "pagination-previous",
+      "pagination-next",
+    ];
+
+    for (let i = 0; i < pagination.children.length; ) {
+      let foundAttr = false;
+
+      paginationAttributes.forEach((attr) => {
+        if (pagination.children[i].attributes.getNamedItem(attr) !== null) {
+          foundAttr = true;
+        }
+      });
+      if (foundAttr === false) {
+        pagination.removeChild(pagination.children[i]);
+      } else {
+        i++;
+      }
+    }
+
+    for (let i = 0; i < numberOfPages; i++) {
+      let child = document.createElement("li");
+      child.classList.add("page-item");
+      if (i === numberOfPages - 1) {
+        child.classList.add("active");
+      }
+      let insideChild = document.createElement("a");
+      insideChild.classList.add("page-link");
+      let textnode = document.createTextNode(numberOfPages - i);
+      insideChild.appendChild(textnode);
+      insideChild.setAttribute("href", "#!");
+      child.appendChild(insideChild);
+      child.onclick = function () {
+        if (chunk !== numberOfPages - i) {
+          chunk = numberOfPages - i;
+          onClickPaginationButtonitz4rs(chunk, pagination);
+        }
+      };
+
+      let numberOfFrontButtons =  findTypeOfPaginationitz4rs(pagination);
+      pagination.insertBefore(child, pagination.children[numberOfFrontButtons]);
+
+    }
+
+    for (let i = 0; i < pagination.children.length; i++) {
+      let child = pagination.children[i];
+      if ( child.getAttribute("pagination-first") === "true" && numberOfPages > 0 ) {
+        child.onclick = function () {
+          if(chunk !== 1){
+            chunk = 1;
+            onClickPaginationButtonitz4rs(1, pagination);
+          }
+        };
+      }
+
+      if ( child.getAttribute( "pagination-last" ) === "true" && numberOfPages > 0 ) {
+        child.onclick = function () {
+          if(chunk !== numberOfPages){
+            chunk = numberOfPages;
+            onClickPaginationButtonitz4rs( numberOfPages, pagination);
+          }
+        };
+      }
+
+      if ( child.getAttribute("pagination-previous") === "true" && numberOfPages > 0 ) {
+        child.onclick = function () {
+          if(chunk !== returnChunkIndexitz4rs(chunk,numberOfPages,'-')){
+            chunk = returnChunkIndexitz4rs(chunk,numberOfPages,'-');
+            onClickPaginationButtonitz4rs(chunk, pagination);
+          }
+        };
+      }
+
+      if ( child.getAttribute("pagination-next") === "true" && numberOfPages > 0) {
+        child.onclick = function () {
+          if(chunk !== returnChunkIndexitz4rs(chunk,numberOfPages,'+')){
+            chunk = returnChunkIndexitz4rs(chunk,numberOfPages,'+');
+            onClickPaginationButtonitz4rs(chunk, pagination);
+          }
+        };
+      }
+    };
     [...subDataElements].forEach((element,index) => {if(index >= data.length) subDataElements[index].style.display = 'none';})}});};
